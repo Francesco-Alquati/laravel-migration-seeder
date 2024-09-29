2d02e4373f28dc2c770b9a3900282628572288dd
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-30 00:02:04 +0200

    [UPD] Create a new Controller as TrainsController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TrainsController as TrainController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/trains', [TrainsController::class, 'index'])->name('trains');
